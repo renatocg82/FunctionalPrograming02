09949d2b278503861d17e390df7e325c0f18749f
renatocg82 <66907169+renatocg82@users.noreply.github.com>
2023-08-13 20:27:42 -0300

    Function as a first class object

---
+++ src/application/App002.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class App002 {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		list.forEach(System.out::println);
+		System.out.println("-------------");
+		
+		list.sort(App002::compareProducts);
+		list.forEach(System.out::println);
+	}
+
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
+}
